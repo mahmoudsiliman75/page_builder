3951189547dd531d096b47bf5eeca8f490fd0452
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-06-15 14:53:35 +0200

    set the project structure

--- layouts/TheFooter.php
+++ layouts/TheFooter.php
@@ -1,91 +1,9 @@
 <!-- START:: FOOTER -->
 <footer>
-  <!-- START:: UPPER SECTION SVG -->
-  <div class="upper_svg_wraper">
-    <img src="assets/media/shapes/shp01.png" class="curve">
-  </div>
-  <!-- END:: UPPER SECTION SVG -->
-
   <div class="container-fluid">
     <div class="row justify-content-center">
-      <div class="footer_logo col-6 col-md-2"> 
-        <a href="index.php">
-          <img src="assets/media/logo.png" alt="The Logo">
-        </a>
-      </div>
-
-      <div class="short_cuts col-12 col-md-4 d-flex justify-content-between justify-content-md-center"> 
-        <div class="d-flex flex-column align-items-start justify-content-center p-3">
-          <a href="index.php"> Home </a>
-          <a href="AboutUs.php"> About Us </a>
-          <a href="AllOperations.php"> Operations </a>
-        </div>
-
-        <div class="d-flex flex-column align-items-start justify-content-center p-3">
-          <a href="AllVideos.php"> Videos </a>
-          <a href="AllArticles.php"> Aticles </a>
-          <a href="ContactUs.php"> Contact Us </a>
-        </div>
-      </div>
-
-      <div class="get_in_touch col-10 col-md-3 p-0 px-md-2"> 
-        <h5 class="my-3 d-flex"> Contact Us </h5>
-
-        <ul class="list-unstyled p-0">
-          <li class="d-flex align-items-center"> 
-            <i class="fas fa-map-marker-alt mx-1"></i>
-            <span> Address Will Be Desplaied Here </span>
-          </li>
-
-          <li class="d-flex align-items-center"> 
-            <i class="fas fa-envelope mx-1"></i>
-            <span> 
-              <a href="mailto: info@Al-koubasy.com"> info@P-Cure.com </a> </span>
-          </li>
-
-          <li class="d-flex align-items-center"> 
-            <i class="fas fa-mobile-alt mx-1"></i>
-            <span> 
-              <a href="tele:01000000001">01000000001 </a>
-              - 
-              <a href="tele:01000000002">01000000002 </a> 
-            </span>
-          </li>
-        </ul>
-      </div>
-
-      <div class="follow_us col-12 col-md-3"> 
-        <h5 class="text-center my-3"> Follow Us </h5>
-        <ul class="list-unstyled">
-          <li> 
-            <a href="#" class="facebook" target="_blanck"> 
-              <img src="assets/media/icons/social/facebook.png">
-            </a>
-          </li>
-
-          <li> 
-            <a href="#" class="instagram" target="_blanck"> 
-              <img src="assets/media/icons/social/instagram.png">
-            </a>
-          </li>
-
-          <li> 
-            <a href="#" class="youtube" target="_blanck"> 
-              <img src="assets/media/icons/social/youtube.png">
-            </a>
-          </li>
-
-          <li> 
-            <a href="#" class="twitter" target="_blanck"> 
-              <img src="assets/media/icons/social/twitter.png">
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div class="copy_rights col-12 d-flex justify-content-between">
-        <span> Powered By <a href="https://www.facebook.com/MediaServAgency/" target="_blank"> MediaServ </a> </span>
-        <span> &copy; 2021 P Cure </span>
+      <div class="copy_rights col-12 d-flex justify-content-center">
+        <span> Created With <i class="fas fa-heart text-danger"></i> By <a href="https://www.technomasr.com/" target="_blank"> Technomasr </a> </span>
       </div>
     </div>
   </div>
